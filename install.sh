ae5a419705dc12900038b1ca966fa589908a8bb4
rafay99-epic <99marafay@gmail.com>
2022-09-22 21:52:05 +0500

    Permission Section
    In this Patch of the code we are adding a couple of thing.
    1. Adding Files
    2. A permission Scripts that will provide permissions to the other scrips.
    3. Combining all the scripts into install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # base of the script
 
@@ -13,8 +13,34 @@ echo -ne "
                                                                                                                                                                                                                                                                               
 "
 
+# Running  The permission script
+cd permissions
+chmod +x permission.sh
+./permission.sh
+cd ../
+
+# Running the  internet connection script
+cd connection
+./net-connection.sh
+cd ../
+
+#Running the Os script
+cd OS
+./os.sh
+cd ../
+
+# Running the reboot script
+cd reboot
+./reboot.sh
+cd ../
+
 echo -ne "
------------------------------------------------------------------------------
-|                                                                           |  
------------------------------------------------------------------------------
+
+ ██████╗  ██████╗  ██████╗ ██████╗     ██████╗ ██╗   ██╗███████╗
+██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗    ██╔══██╗╚██╗ ██╔╝██╔════╝
+██║  ███╗██║   ██║██║   ██║██║  ██║    ██████╔╝ ╚████╔╝ █████╗  
+██║   ██║██║   ██║██║   ██║██║  ██║    ██╔══██╗  ╚██╔╝  ██╔══╝  
+╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝    ██████╔╝   ██║   ███████╗
+ ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝     ╚═════╝    ╚═╝   ╚══════╝
+                                                                
 "
--- permissions/permission.sh
+++ permissions/permission.sh
@@ -1,1 +1,74 @@
-#!bin/bash
+#!/bin/bash
+
+echo  -ne "
+
+██████╗ ███████╗██████╗ ███╗   ███╗██╗███████╗███████╗██╗ ██████╗ ███╗   ██╗███████╗
+██╔══██╗██╔════╝██╔══██╗████╗ ████║██║██╔════╝██╔════╝██║██╔═══██╗████╗  ██║██╔════╝
+██████╔╝█████╗  ██████╔╝██╔████╔██║██║███████╗███████╗██║██║   ██║██╔██╗ ██║███████╗
+██╔═══╝ ██╔══╝  ██╔══██╗██║╚██╔╝██║██║╚════██║╚════██║██║██║   ██║██║╚██╗██║╚════██║
+██║     ███████╗██║  ██║██║ ╚═╝ ██║██║███████║███████║██║╚██████╔╝██║ ╚████║███████║
+╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚══════╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+                                                                                
+"
+echo -ne "
+-----------------------------------------------------------------------------
+|             --- Giving Permission to Shell scripts ---                    |
+-----------------------------------------------------------------------------
+"
+# Permissions fot the Arch Linux System 
+# Permisison for the deep learning Section
+cd ../
+cd arch
+cd deep-learning
+chmod +x arch-deep-learning.sh
+chmod +x arch-deep-compoents.sh
+
+# Permission for the arch web development Section
+cd ../
+cd web-development
+chmod +x arch-web-development.sh
+chmod +x arch-web-development-compoents.sh
+
+cd ../
+cd ../
+
+# Net Connection script Permission Section
+
+cd connection
+chmod +x net-connection.sh
+cd ../
+
+# Permission for Debian System
+
+# Deep Learning
+cd debian
+cd deep-learning
+chmod +x debian-deep-learning.sh
+chmod +x debian-deep-compoent.sh
+
+# Web Development
+cd ../
+cd web
+chmod +x debian-web-development.sh
+chmod +x debian-web-compoent.sh
+
+cd ../
+cd ../
+
+
+# Permssion for the Os detection section
+cd OS
+chmod +x os.sh
+cd ../
+
+# Permission for the Reboot section
+cd reboot
+chmod +x reboot.sh
+cd ../
+
+
+
+
+
+
+
